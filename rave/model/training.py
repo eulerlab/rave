2f9b433d66925a9b2e92d75e523ab794252d7733
Larissa Hoefling <larissa.hoefling@uni-tuebingen.de>
2021-10-26 12:48:57 +0200

    Working version of batched training

--- rave/model/training.py
+++ rave/model/training.py
@@ -3,8 +3,9 @@ import pickle as pkl
 import numpy as np
 from ray import tune
 import torch
+import time
 from torch import nn
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, BatchSampler, SequentialSampler
 from sklearn.ensemble import RandomForestClassifier as RFC
 from sklearn.model_selection import ParameterGrid, GridSearchCV
 from scipy.stats import spearmanr
@@ -14,6 +15,10 @@ from rave.model.utils import *
 from typing import Dict, AnyStr
 from rave.utils.import_helpers import dynamic_import, split_module_name
 from rave.data.datasets import get_bc_data, get_bc_sim_data
+
+
+def custom_collate_fn(input_list):
+    return [el for el in input_list[0]]
 
 
 def train_model(config: Dict):
@@ -74,10 +79,10 @@ def train_model(config: Dict):
     lr_discriminator = config["training"].get("lr_discriminator")
     weight_decay = config["training"].get("weight_decay")
     train_model_every = config["training"].get("train_model_every")
-    num_epochs = config["training"].get("num_epochs")
+    num_iter = config["training"].get("num_epochs")
     log_dir = config["training"].get("log_dir")
     log_steps = config["training"].get("log_steps")
-    weight_recon = config["training"]["weight_recon"]
+    weight_mse = config["training"]["weight_recon"]
     weight_type = config["training"]["weight_type"]
     weight_scan = config["training"]["weight_scan"]
     batch_size = config["training"]["batch_size"]
@@ -95,18 +100,18 @@ def train_model(config: Dict):
     domain_classification_loss_fn = dynamic_import(
         *split_module_name(domain_classification_loss_str)
     )
-
+    no_adv = False
     adversarial_training = True
     if dis_warm_cool_steps > 0:
         adversarial_training = False
-
+        no_adv = True
     if config["supervised"]:
-        weight_recon = 0.0
+        weight_mse = 0.0
     else:
         weight_type = 0.0
 
     if train_model_every > 1:
-        num_epochs *= train_model_every
+        num_iter *= train_model_every
 
     var_exp_train, var_exp_val = [], []
     adv_acc_train, adv_acc_val = [], []
@@ -116,79 +121,87 @@ def train_model(config: Dict):
     optim_step_count = 1
     # ToDo: Figure out what optim_step_count should be
     print("adversarial training: {}".format(adversarial_training))
-    for epoch in range(num_epochs):
-        for batch_no, (x, y_scan, y_type) in enumerate(
-                DataLoader(dataset, batch_size, shuffle=True)
+    t0 = time.time()
+    for i in range(num_iter + 1):
+        # Model Training
+        for batch_no, (x_train, y_scan_train, y_type_train) in enumerate(
+                DataLoader(dataset,
+                           sampler=BatchSampler(SequentialSampler(dataset),
+                                                batch_size, drop_last=False),
+                           shuffle=False,
+                           collate_fn=custom_collate_fn)
         ):
-            x = x.to(config["device"])
-            y_scan = y_scan.to(config["device"]).long()
-            y_type = y_type.to(config["device"]).long()
-            z, y_scan_hat, y_type_hat, x_rec = net(x)
-            loss = get_loss(dataset, x, y_scan, y_type, x_rec, y_scan_hat, y_type_hat,
-                            weight_recon, weight_scan, weight_type,
-                            reconstruction_loss_fn,
+            optimizer_model.zero_grad()
+            z, y_scan, y_type, x_rec = net(x_train)
+            loss, loss_mse, loss_type = get_loss(dataset, x_train, y_scan_train, y_type_train,
+                            x_rec,
+                            y_scan, y_type, weight_mse, weight_scan,
+                            weight_type, reconstruction_loss_fn,
                             type_classification_loss_fn,
-                            domain_classification_loss_fn,
-                            adversarial_training
-                            )
+                            domain_classification_loss_fn, adversarial_training)
             loss.backward()
-            if not epoch % train_model_every and check_grad(net):
-                # if (batch_no+1) % optim_step_count == 0:
+            if not i % train_model_every and check_grad(net):
                 optimizer_model.step()
-                optimizer_model.zero_grad()
-            z, y_scan_hat, y_type_hat, x_rec = net(x)
-            loss_adv = get_adv_loss(dataset, y_scan, y_scan_hat, weight_scan,
-                                    domain_classification_loss_fn)
+
+            # Adversarial Discriminator
+            optimizer_discriminator.zero_grad()
+            z, y_scan, y_type, x_rec = net(x_train)
+            # compute Adversarial Classification Loss per Scan:
+
+            loss_adv = get_adv_loss(dataset, y_scan_train, y_scan, domain_classification_loss_fn)
             loss_adv.backward()
             if check_grad(net):
                 optimizer_discriminator.step()
-                optimizer_discriminator.zero_grad()
-        net.eval()
-        z, y_scan_hat, y_type_hat, x_rec = net(x_train)
-        var_exp_train.append(var_exp(total_variance_train, x_train, x_rec))
-        net.train()
-        if not epoch % log_steps:
-            print(epoch)
+
+        if not (i % log_steps):
             torch.save(net.state_dict(), os.path.join(log_dir, 'model.pth'))
+
+            # run_type_acc += accuracy
+            print('step=%s, total time=%.2fm' % (i, (time.time() - t0) / 60))
+            # run_ve /= log_steps
+            # run_adv_acc /= log_steps
+            # run_type_acc /= log_steps
+
+            # validate
+            # steps_val.append(i)
             net.eval()
-            z_val, y_scan_val_pred, y_type_val_pred, x_rec_val = \
-                net(x_val.to(config["device"]))
-            corr_val = correlate(x_val, x_rec_val)
-            predicted = torch.argmax(y_scan_val_pred, dim=-1)
+            z, y_scan, y_type, x_rec = net(x_val)
+            res = (x_val - x_rec) ** 2
+            ve = torch.mean(1 - torch.sum(res, 1) / total_variance_val).item()
+            var_exp_val.append(ve)
+            _, predicted = torch.max(y_scan, 1)
             accuracy_scan = (predicted == y_scan_val).double().mean().item()
             adv_acc_val.append(accuracy_scan)
-            predicted = torch.argmax(y_type_val_pred, dim=-1)
-            accuracy_type = (
-                    predicted[y_scan_val==0] == y_type_val[y_scan_val==0]
-            ).double().mean().item()
-
-            accuracy_type_unsup = (
-                    predicted[y_scan_val == 1] == (y_type_val[y_scan_val == 1])*-1 -1
-            ).double().mean().item()
-
+            _, predicted = torch.max(y_type, 1)
+            accuracy_type = (predicted == y_type_val).double().mean().item()
             type_acc_val.append(accuracy_type)
-            var_exp_val.append(var_exp(total_variance_val, x_val, x_rec_val))
-            print("corr: {:.2f}, scan_acc: {:.2f}, type_acc: {:.2f}, "
-                  "type_acc B:{:.2f}, var_exp: {:.2f}".format(
-                corr_val, accuracy_scan, accuracy_type, accuracy_type_unsup,
-                var_exp_val[-1]
-            ))
-            if hypersearch:
-                tune.report(
-                    iteration=epoch,
-                    var_exp=var_exp_val[-1],
-                    corr_val=corr_val,
-                    adv_acc=accuracy_scan,
-                    type_acc=accuracy_type,
-                    final_scan_acc=0,
-                    final_type_acc=0,
-                    lsq_var_exp=0,
-                    lsq_corrs=0
-                )
+            # log = 'Train: var.exp.=%.2f, acc_adv=%.2f, acc_type=%.2f; mse=%.2f' % (
+            #     run_ve, run_adv_acc, run_type_acc, loss_mse)
+            log = ' Val: var.exp.=%.2f, acc_adv=%.2f, acc_type=%.2f; mse=%.2f' % (
+                ve, accuracy_scan, accuracy_type, loss_mse)
+            print(log)
+            run_ve = 0.0
+            run_adv_acc = 0.0
+            run_type_acc = 0.0
             net.train()
 
-        if epoch > dis_warm_cool_steps:
-            adversarial_training = True
+        if i > dis_warm_cool_steps:
+            if num_iter - dis_warm_cool_steps > i:
+                if no_adv:
+                    print('\nBeginning 2 player game:\n')
+                    no_adv = False
+                    optimizer_model, optimizer_discriminator = get_optimizers(
+                        net, optimizer, lr_model, lr_discriminator, weight_decay)
+            else:
+                if not no_adv:
+                    print('\nCooling down (no adversarial training):\n')
+                    no_adv = True
+                    weight_type = 0
+                    weight_mse = 0
+                    weight_scan = 0
+                    optimizer_net, optimizer_discriminator = get_optimizers(
+                        net, optimizer, lr_model, lr_discriminator,
+                        weight_decay)
 
     net.eval()
     if hypersearch:
@@ -196,22 +209,22 @@ def train_model(config: Dict):
             net, dataset, x_train, x_val, total_variance_val, y_scan_train,
             y_type_train, y_scan_val, y_type_val)
 
-        final_eval_results.update(dict(iteration=epoch))
-        tune.report(iteration=epoch,
-                    var_exp=final_eval_results["var_exp"],
-                    val_corr=final_eval_results["val_corr"],
-                    adv_acc=final_eval_results["adv_acc"],
-                    type_acc=final_eval_results["type_acc"],
-                    final_scan_acc=final_eval_results["final_scan_acc"],
-                    final_type_acc=final_eval_results["final_type_acc"],
-                    lsq_var_exp=final_eval_results["lsq_var_exp"],
-                    lsq_corrs=final_eval_results["lsq_corrs"])
-        with open(os.path.join(log_dir, 'grid_search_type.pkl'), 'wb') as handle:
-            pkl.dump(grid_search_type.best_params_,
-                     handle, protocol=pkl.HIGHEST_PROTOCOL)
-        with open(os.path.join(log_dir, 'grid_search_scan.pkl'), 'wb') as handle:
-            pkl.dump(grid_search_scan.best_params_,
-                     handle, protocol=pkl.HIGHEST_PROTOCOL)
+        # final_eval_results.update(dict(iteration=epoch))
+        # tune.report(iteration=epoch,
+        #             var_exp=final_eval_results["var_exp"],
+        #             val_corr=final_eval_results["val_corr"],
+        #             adv_acc=final_eval_results["adv_acc"],
+        #             type_acc=final_eval_results["type_acc"],
+        #             final_scan_acc=final_eval_results["final_scan_acc"],
+        #             final_type_acc=final_eval_results["final_type_acc"],
+        #             lsq_var_exp=final_eval_results["lsq_var_exp"],
+        #             lsq_corrs=final_eval_results["lsq_corrs"])
+        # with open(os.path.join(log_dir, 'grid_search_type.pkl'), 'wb') as handle:
+        #     pkl.dump(grid_search_type.best_params_,
+        #              handle, protocol=pkl.HIGHEST_PROTOCOL)
+        # with open(os.path.join(log_dir, 'grid_search_scan.pkl'), 'wb') as handle:
+        #     pkl.dump(grid_search_scan.best_params_,
+        #              handle, protocol=pkl.HIGHEST_PROTOCOL)
 
     results = {
         'var_exp_train': var_exp_train,
